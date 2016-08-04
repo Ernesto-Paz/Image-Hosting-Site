12e6a7e3fe2d38a27c718874b6f0ff677d30737a
Ernesto Paz <Ernesto-Paz@users.noreply.github.com>
2016-08-03 22:06:04 -0400

    Used express generator to create a barebones server. Added a text files that holds expected User Stories for each stage of development.

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const util = require("util");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-router.post("/upload", function(req, res, next){
-var thedata = "";
-    
-request.on("data", function(data){
-console.log("Data get");
-console.log(data);
-thedata += data
-})
-console.log(util.inspect(req.body));
-request.on("end",function(){
-res.send(thedata);
-})
-console.log(thedata);
-console.log("Finished.");
 });
 
 module.exports = router;
